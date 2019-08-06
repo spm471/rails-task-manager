1539faae2468acebc71af1326186197265934371
SebastianoPio Matera <spm471@nyu.edu>
2019-08-06 13:26:53 +0200

    Done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
